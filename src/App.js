4d1f9e05bbbbe571583df773521e02d226c91115
renan <renan.djr@hotmail.com>
2022-08-29 18:20:30 -0300

    Page 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Box, } from '@chakra-ui/react'
 
 function App() {
   return (
     
-   <div className="bg">
-    
-   </div>
+    <Box h='1024px' w='1444px' bgGradient='linear(to-br, #FF4343, #810000)'>
+      
+      <Box position='absolute' width='414px' height='77px' left='419px' top='395px' fontFamily='Inter' fontStyle='normal' fontWeight='500' fontSize='64px' lineHeight='77px' color='white'>
+        Bem vindo ao
+      </Box>
+      <Box position='absolute' w='602px' h='116px' left='419px' top='452px' fontFamily='Inter' fontStyle='normal' fontWeight='700'
+      fontSize='96px' lineHeight='116px' color='white' >
+        Não Briguem
+      </Box>
+
+     
+    </Box>
    
   );
 }
