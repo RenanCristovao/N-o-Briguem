0e303306ac8c24ddf98a6bd9f4da7a4b62bd6239
renan <renan.djr@hotmail.com>
2022-08-29 11:43:05 -0300

    Create page1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import './App.css';
+
 
 function App() {
   return (
     
    <div className="bg">
+    <div className="text1">
+      Bem vindo ao
+    </div>
+    <div className="text2">
+      Não Briguem
+    </div>
     
    </div>
    
