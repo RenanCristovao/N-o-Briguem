2f7918b0b1a4ab73a7a9e3fa6355556aa8c4ff54
renan <renan.djr@hotmail.com>
2022-09-02 14:03:04 -0300

    Ajusts page NaoBriguem

--- src/page/NaoBriguem.js
+++ src/page/NaoBriguem.js
@@ -1,14 +1,54 @@
-import { Flex, Text} from '@chakra-ui/react'
+import { Flex, Text, Box, Button} from '@chakra-ui/react'
+
 
 function NaoBriguem() {
   return (
-    
+
     <Flex h='100vh' w='100vw' bgGradient='linear(to-br, #FF4343, #810000)' justify={'center'} align='center' >
 
-      <Flex color='white' direction='column'>
+      <Flex color='white' direction='column'  align='center' >
         <Text fontSize='64px' as='b' >
-          Pagina 2
+          Vocês estão a
         </Text>
+        <Flex >
+          <Box fontSize='32px' align='center' margin='0 13px'>
+            <Box bg='white'  borderRadius='100%' p='7' w='150px' h='150px'  boxShadow='10px 5px 4px rgba(0, 0, 0, 0.25)' >
+              <Text color='#B22020' fontSize='64px' as='b'  >
+                15
+              </Text>
+            </Box>
+              Dias
+          </Box> 
+          <Box fontSize='32px' align='center' margin='0 13px'>
+            <Box bg='white'  borderRadius='100%' p='7' w='150px' h='150px'  boxShadow='10px 5px 4px rgba(0, 0, 0, 0.25)' >
+              <Text color='#B22020' fontSize='64px' as='b'  >
+                20
+              </Text>
+            </Box>
+              Horas
+          </Box> 
+          <Box fontSize='32px' align='center' margin='0 13px'>
+            <Box bg='white'  borderRadius='100%' p='7' w='150px' h='150px'  boxShadow='10px 5px 4px rgba(0, 0, 0, 0.25)' >
+              <Text color='#B22020' fontSize='64px' as='b'  >
+               45
+              </Text>
+            </Box>
+              Minutos
+          </Box> 
+          <Box fontSize='32px' align='center' margin='0 13px'>
+            <Box bg='white'  borderRadius='100%' p='7' w='150px' h='150px'  boxShadow='10px 5px 4px rgba(0, 0, 0, 0.25)' >
+              <Text color='#B22020' fontSize='64px' as='b' justifySelf={'center'} >
+                12
+              </Text>
+            </Box>
+              Segundos
+          </Box> 
+         
+        </Flex>
+        <Text fontSize='64px' as='b' >
+          Sem Brigar
+        </Text>
+        <Button color='#B22020'>Zerar Contador</Button>
       </Flex>
     </Flex>
    
