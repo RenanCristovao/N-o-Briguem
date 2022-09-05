e50a44bde2438021d21bfcf07080e07839ce49fb
renan <renan.djr@hotmail.com>
2022-09-05 17:27:24 -0300

    create breackpoint base, sm and md

--- src/components/CardTimer.js
+++ src/components/CardTimer.js
@@ -3,19 +3,23 @@ import { Box, Text, Flex } from "@chakra-ui/react";
 function CardTimer(props) {
   return (
     <Box
-      fontSize={{ base: "18px", md: "32px" }}
+      fontSize={{ base: "8px", sm: "24px", md: "32px" }}
       textAlign="center"
       margin="0 13px"
     >
       <Box
         bg="white"
         borderRadius="100%"
-        p={{ base: "1", md: "7" }}
-        w={{ base: "75px", md: "150px" }}
-        h={{ base: "75px", md: "150px" }}
+        p={{ base: "0", sm: "2", md: "7" }}
+        w={{ base: "50px", sm: "75px", md: "150px" }}
+        h={{ base: "50px", sm: "75px", md: "150px" }}
         boxShadow="10px 5px 4px rgba(0, 0, 0, 0.25)"
       >
-        <Text color="#B22020" fontSize={{ base: "44px", md: "64px" }} as="b">
+        <Text
+          color="#B22020"
+          fontSize={{ base: "32px", sm: "36px", md: "64px" }}
+          as="b"
+        >
           {props.value}
         </Text>
       </Box>
--- src/page/NaoBriguem.js
+++ src/page/NaoBriguem.js
@@ -11,7 +11,7 @@ function NaoBriguem() {
       align="center"
     >
       <Flex color="white" direction="column" align="center">
-        <Text fontSize="64px" as="b">
+        <Text fontSize={{ base: "32px", md: "64px" }} as="b">
           Vocês estão a
         </Text>
         <Flex>
@@ -20,7 +20,7 @@ function NaoBriguem() {
           <CardTimer value="22" label="Minutos" />
           <CardTimer value="10" label="Segundos" />
         </Flex>
-        <Text fontSize="64px" as="b" align="center">
+        <Text fontSize={{ base: "32px", md: "64px" }} as="b" align="center">
           Sem Brigar
         </Text>
         <Button color="#B22020">Zerar Contador</Button>
